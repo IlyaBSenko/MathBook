c3505cdf8481a370727e93abe11d0f1d12374f71
Ilya Balatsenko <senkoet@gmail.com>
2025-02-11 22:47:42 -0500

    Added the rest of the base definitions, will add more for imaginary/real numbers.

--- definitions.py
+++ definitions.py
@@ -12,7 +12,14 @@ properties_definitions = {
     "Prime numbers": "A number is prime if it can only be divided by itself and 1.",
     "Composite": "A composite number is a positive integer that has more than two factors. They are the opposite of prime numbers.",
     "Perfect": "A perfect number is a number that is equal to the sum of its factors other than the number itself.",
-    "Perfect Square": "A perfect square is a number that can be expressed as the product of an integer by itself or as the second exponent of an integer."
+    "Perfect Square": "A perfect square is a number that can be expressed as the product of an integer by itself or as the second exponent of an integer.",
+    "Sublime numbers": "A sublime number is a positive integer that has a perfect number of positive factors, and the sum of those factors is also a perfect number. For example, 12 is a sublime number because it has six positive factors (1, 2, 3, 4, 6, and 12), and the sum of those factors is 28, which is also a perfect number.",
+    "Triangular Numbers": "A triangular number fits into this series of numbers (1, 3, 6, 10, 15, etc.) obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc. Usually can be in the shape of a triangle.",
+    "Palindromic number": "A number is palindromic if it remains the same when the digits are reversed.",
+    "Armstrong number": "An Armstrong number is a number that is equal to the sum of its own digits, each raised to the power of the number of digits in that number; essentially, it's a number where the sum of its digits raised to their own place value equals the original number itself.",
+    "Automorphic number": "An automorphic number is an integer whose square ends with the same digits as the original number. For example, 25 squared is 625, so 25 is an automorphic number.",
+    "Abundant numbers": "In number theory, an abundant number is a positive integer where the sum of its proper divisors is greater than the number itself. The difference between the sum and the number is called the abundance.",
+    "Deficient numbers": "A deficient number is a positive integer that is not a perfect or abundant number, and where the sum of its proper divisors is less than the number itself. They are essentially the opposite of abundant numbers."
     
     
 }
@@ -27,10 +34,5 @@ def get_property_definition(property_name):
 # Order to add (Based on Size/Relevance)
 
 # Square
-# sublime
-# triangular
-# palindrome
-# armstrong
-# automorphic
-# abundant
-# deficient
+# imaginary numbers
+# real numbers
